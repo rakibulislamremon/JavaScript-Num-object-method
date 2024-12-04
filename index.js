5db4de065fc47e50711d10bfa6239f5d19072b95
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-04 20:05:08 +0600

    Number object method

---
+++ index.js
@@ -0,0 +1,36 @@
+//isfinit method
+var num=7.5
+console.log(
+    Number.isFinite(num)
+)
+
+//isInterger method
+var num=100
+console.log(
+    Number.isInteger(num)
+)
+
+//parsfloat method
+var num=100
+console.log(
+    Number.parseFloat(num)
+)
+
+
+//parsint method
+var num=9.3
+console.log(
+    Number.parseInt(num)
+)
+
+//tofixed method
+var num=9.32907189267639827381
+console.log(
+    num.toFixed(3)
+)
+
+//toString method
+var num=9.32907189267639827381
+console.log(
+    num.toString()
+)
